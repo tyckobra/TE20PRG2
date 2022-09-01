02da0c1c0cc65623ca6f665fc8ce17685f2e51b7
tyckobra <77719515+tyckobra@users.noreply.github.com>
2022-09-01 13:36:15 +0200

    yuuh

---
+++ src/xor.java
@@ -0,0 +1,12 @@
+public class xor {
+    public static void main(String[] args) {
+        int m = 701;
+        int k = 263;
+        int c = m^k;
+        System.out.println(Integer.toBinaryString(m));
+        System.out.println(Integer.toBinaryString(k));
+        System.out.println(Integer.toBinaryString(c));
+        System.out.println(c);
+        System.out.println((char)(c));
+    }
+}
