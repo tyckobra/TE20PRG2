b20a620e479bdc6bac1c71eada651bcbd513417c
tyckobra <77719515+tyckobra@users.noreply.github.com>
2022-09-02 13:36:26 +0200

    yuuh

--- src/xor.java
+++ src/xor.java
@@ -1,12 +1,21 @@
 public class xor {
     public static void main(String[] args) {
-        int m = 701;
-        int k = 263;
+        int m = 738;
+        int k = 193;
         int c = m^k;
         System.out.println(Integer.toBinaryString(m));
         System.out.println(Integer.toBinaryString(k));
         System.out.println(Integer.toBinaryString(c));
         System.out.println(c);
         System.out.println((char)(c));
+
+        for (int a = 738;a <= 1000,m++){
+            System.out.println(c);
+        }
+
+        int crypt(int m, int k){
+            int c= m^k;
+            return c;
+        }
     }
 }
