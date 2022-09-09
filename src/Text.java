81348453ab0906fc21f60af554a7f86a81a84755
tyckobra <77719515+tyckobra@users.noreply.github.com>
2022-09-09 13:36:36 +0200

    yuuh

--- src/Text.java
+++ src/Text.java
@@ -1,20 +1,44 @@
-import jdk.nashorn.internal.scripts.JO;
-import javax.swing.*;
-import java.awt.event.*;
-import java.util.Scanner;
+import java.util.Random;
 import java.io.*;
-import array.util.list;
+
+import static java.lang.System.out;
 
 public class Text {
     public static void main(String[] args) throws IOException {
-        String Tycko = "Tycko.txt";
-        FileWriter fw = new FileWriter(Tycko);
+
+        File ok = new File("Tycko.txt");
+
+        DataInputStream input = new DataInputStream (new BufferedInputStream(new FileInputStream("myData.dat")));
+        DataOutputStream output = new DataOutputStream (new BufferedOutputStream(new FileOutputStream("myData.dat")));
+
+        String ty = "Tycko.txt";
+        FileWriter fw = new FileWriter(ty);
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter utFil = new PrintWriter(bw);
-        FileReader fr = new FileReader(Tycko);
-        BufferedReader inFil = new BufferedReader(fr);
 
-        Scanner input = new Scanner(new File("Tycko.txt"));
+        DataInputStream inFile = input;
+        int number = Integer.parseInt(inFile.readLine());
+        DataOutput outFile = output;
+        outFile.writeInt(number);
+
+        input.readInt();
+        output.writeInt(new FileWriter());
+
+        int y;
+        int x;
+        Random r = new Random();
+
+        for (int i = 0 ; i < 1920*1080 ; i++) {
+            x = r.nextInt(0xffffff);
+            y = r.nextInt(0x16775215);
+            out.println(x^y);
+            try {
+                x = Integer.parseInt(String.valueOf(new PrintWriter((File) outFile)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
